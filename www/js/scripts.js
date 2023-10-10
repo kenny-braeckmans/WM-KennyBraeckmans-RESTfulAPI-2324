719561e2e5212f1c94f86fbe4e03c56a3f7e398c
Kenny Braeckmans <kenny.braeckmans@student.odisee.be>
2023-10-10 19:04:26 +0200

    add simple frontend showing projects

---
+++ www/js/scripts.js
@@ -0,0 +1,87 @@
+/**
+ * Exercise: RESTful API
+ * 
+ * @description This script fetches and displays current weather data for Brussels.
+ * @author Kenny Braeckmans <kenny.braeckmans@student.odisee.be>
+ */
+
+console.log('😺');
+
+const baseUrl = 'https://bennykraeckmans.be';
+const endpoints = {
+    projects:  '/api/v1/projects',
+    employees: '/api/v1/employees'
+};
+
+function buildUrl(baseUrl, endpoint, params = {}) {
+    let url = baseUrl + endpoint;
+
+    if (Object.keys(params).length > 0) {
+        url += '?';
+
+        for (const [key, value] of Object.entries(params)) {
+            url += `${key}=${value}&`;
+        }
+
+        url = url.slice(0, -1);
+    }
+
+    return url;
+}
+
+async function fetchProjects() {
+    console.log('[+] fetchProjects');
+    const url = buildUrl(baseUrl, endpoints.projects);
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function addProjectsTable(json, element) {
+    console.log('[+] addProjectsTable');
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    const headers = ['Id', 'Name', 'Code', 'Description'];
+    const headerRow = document.createElement('tr');
+
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.innerText = header;
+        headerRow.appendChild(th);
+    });
+
+    thead.appendChild(headerRow);
+
+    json.forEach(project => {
+        const tr = document.createElement('tr');
+
+        for (const [key, value] of Object.entries(project)) {
+            const td = document.createElement('td');
+            td.innerText = value;
+            tr.appendChild(td);
+        }
+
+        tbody.appendChild(tr);
+    });
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    element.appendChild(table);
+}
+
+window.addEventListener('load', async () => {
+    try {
+        const projects = await fetchProjects();
+        addProjectsTable(projects, document.querySelector('#container1'));
+    } catch (error) {
+        console.error("[!] Error fetching and displaying projects.", error);
+    }
+});
