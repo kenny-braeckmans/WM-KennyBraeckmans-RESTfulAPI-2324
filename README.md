028d79ef460ed36151c8753cc030a6e7038d1b37
Kenny Braeckmans <kenny.braeckmans@student.odisee.be>
2023-10-13 17:13:07 +0200

    add documentation & clean up

--- README.md
+++ README.md
@@ -1,43 +1,84 @@
 # 1. Exercise RESTful API
 
-## Demo
-- [www.bennykraeckmans.be](https://www.bennykraeckmans.be/)
-- [api.bennykraeckmans.be](https://api.bennykraeckmans.be/)
+## 🔗 Demo
 
-## API testing
+- 🌐 Web: [www.bennykraeckmans.be](https://www.bennykraeckmans.be/)
+- 📡 API: [api.bennykraeckmans.be](https://api.bennykraeckmans.be/)
 
+## 📚 References
+
+### Best practices
+
+- [Mark Roland: How to Build a RESTful API Web Service with PHP](https://web.archive.org/web/20220209214153/https://markroland.com/portfolio/restful-php-api)
+- [Corey Maynard: Creating a RESTful API with PHP](https://web.archive.org/web/20220314015154/http://coreymaynard.com/blog/creating-a-restful-api-with-php/)
+
+### Slim Framework
+
+- [Installation - Slim Framework](https://www.slimframework.com/docs/v4/start/installation.html)
+- [Apache & mod_rewrite - Slim Framework](https://www.slimframework.com/docs/v4/start/web-servers.html#apache-configuration)
+- [Routing - Slim Framework](https://www.slimframework.com/docs/v4/objects/routing.html#how-to-create-routes)
+- [Setting up CORS - Slim Framework](https://www.slimframework.com/docs/v4/cookbook/enable-cors.html)
+
+## 🧪 API Testing
+
+### Projects
+
+GET all projects
+``` sh
+curl -L -X GET https://api.bennykraeckmans.be/v1/projects
+```
+
+GET project with id 1
+``` sh
+curl -L -X GET https://api.bennykraeckmans.be/v1/projects/1
+```
+
+POST new project
+``` sh
+curl -L -X POST -H "Content-Type: application/json" \
+-d '{"name":"ProjectName", "code":"ProjectCode", "description":"DescriptionHere"}' \
+https://api.bennykraeckmans.be/v1/projects
+```
+
+PUT updated project with id 420
+``` sh
+curl -L -X PUT -H "Content-Type: application/json" \
+-d '{"name":"ProjectName", "code":"ProjectCode", "description":"Hi mom!"}' \
+https://api.bennykraeckmans.be/v1/projects/420
+```
+
+DELETE project with id 420
+``` sh
+curl -L -X DELETE https://api.bennykraeckmans.be/v1/projects/420
+```
+
+### Employees
+
+GET all employees
 ``` sh
 curl -L -X GET https://api.bennykraeckmans.be/v1/employees
 ```
 
+GET employee with id 1
 ``` sh
 curl -L -X GET https://api.bennykraeckmans.be/v1/employees/1
 ```
 
+POST new employee
 ``` sh
 curl -L -X POST -H "Content-Type: application/json" \
 -d '{"first_name":"Benny", "last_name":"Kraeckmans", "specialization":""}' \
 https://api.bennykraeckmans.be/v1/employees
 ```
 
+PUT updated employee with id 420
 ``` sh
 curl -L -X PUT -H "Content-Type: application/json" \
 -d '{"first_name":"Benny", "last_name":"Kraeckmans", "specialization":"Hi mom!"}' \
 https://api.bennykraeckmans.be/v1/employees/420
 ```
 
+DELETE employee with id 420
 ``` sh
 curl -L -X DELETE https://api.bennykraeckmans.be/v1/employees/420
 ```
-
-## References
-
-### Best practices
-- [Mark Roland: How to Build a RESTful API Web Service with PHP](https://web.archive.org/web/20220209214153/https://markroland.com/portfolio/restful-php-api)
-- [Corey Maynard: Creating a RESTful API with PHP](https://web.archive.org/web/20220314015154/http://coreymaynard.com/blog/creating-a-restful-api-with-php/)
-
-### Slim Framework
-- [Installation - Slim Framework](https://www.slimframework.com/docs/v4/start/installation.html)
-- [Apache & mod_rewrite - Slim Framework](https://www.slimframework.com/docs/v4/start/web-servers.html#apache-configuration)
-- [Routing - Slim Framework](https://www.slimframework.com/docs/v4/objects/routing.html#how-to-create-routes)
-- [Setting up CORS - Slim Framework](https://www.slimframework.com/docs/v4/cookbook/enable-cors.html)
--- api/api.php
+++ api/api.php
@@ -1,23 +1,38 @@
 <?php
 
+/**
+ * API for the project management app
+ */
+
 // Enable error reporting - REMOVE ME
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Include required files
+// Load Composer's autoloader for required libraries
+require __DIR__ . '/vendor/autoload.php';
+
+// Load the database configuration for the application
+require __DIR__ . '/inc/config.php';
+
+// Import necessary classes using the Composer autoloader
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteContext;
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/inc/config.php';
-
+// Create a new Slim app
 $app = AppFactory::create();
 
+/**
+ * Middleware
+ */
+
+ // This middleware will parse the JSON request body and add the parsed body to the request object
 $app->addBodyParsingMiddleware();
 
-// This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
+// This middleware will add CORS headers to the response
 $app->add(function (Request $request, RequestHandlerInterface $handler): Response {
     $routeContext = RouteContext::fromRequest($request);
     $routingResults = $routeContext->getRoutingResults();
@@ -26,18 +41,19 @@ $app->add(function (Request $request, RequestHandlerInterface $handler): Respons
 
     $response = $handler->handle($request);
 
-    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'https://www.bennykraeckmans.be');
     $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
     $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
-
-    // Optional: Allow Ajax CORS requests with Authorization header
-    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
 
     return $response;
 });
 
 // The RoutingMiddleware should be added after our CORS middleware so routing is performed first
 $app->addRoutingMiddleware();
+
+/**
+ * Routes for API endpoints
+ */
 
 // Get all projects
 $app->get('/v1/projects', function (Request $request, Response $response) use ($mysqli) {
@@ -53,7 +69,7 @@ $app->get('/v1/projects', function (Request $request, Response $response) use ($
     }
 
     $response->getBody()->write(json_encode($projects, JSON_PRETTY_PRINT));
-    return $response->withHeader('Content-Type', 'application/json');
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
 });
 
 // Get a single project
@@ -70,9 +86,10 @@ $app->get('/v1/projects/{id}', function (Request $request, Response $response, $
 
     if ($project) {
         $response->getBody()->write(json_encode($project, JSON_PRETTY_PRINT));
-        return $response->withHeader('Content-Type', 'application/json');
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
     } else {
-        return $response->withStatus(404)->getBody()->write(json_encode(['error' => 'Project not found']));
+        $response->getBody()->write(json_encode(['error' => 'Project not found']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
     }
 });
 
@@ -88,9 +105,11 @@ $app->post('/v1/projects', function (Request $request, Response $response) use (
     $stmt->bind_param("sss", $name, $code, $description);
 
     if ($stmt->execute()) {
-        return $response->withStatus(201)->getBody()->write(json_encode(['message' => 'Project added successfully']));
+        $response->getBody()->write(json_encode(['message' => 'Project added successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
     } else {
-        return $response->withStatus(500)->getBody()->write(json_encode(['error' => 'Failed to add project']));
+        $response->getBody()->write(json_encode(['error' => 'Failed to add project']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
     }
 });
 
@@ -107,9 +126,11 @@ $app->put('/v1/projects/{id}', function (Request $request, Response $response, $
     $stmt->bind_param("sssi", $name, $code, $description, $id);
 
     if ($stmt->execute()) {
-        return $response->getBody()->write(json_encode(['message' => 'Project updated successfully']));
+        $response->getBody()->write(json_encode(['message' => 'Project updated successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
     } else {
-        return $response->withStatus(500)->getBody()->write(json_encode(['error' => 'Failed to update project']));
+        $response->getBody()->write(json_encode(['error' => 'Failed to update project']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
     }
 });
 
@@ -121,13 +142,106 @@ $app->delete('/v1/projects/{id}', function (Request $request, Response $response
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
-        return $response->getBody()->write(json_encode(['message' => 'Project deleted successfully']));
+        $response->getBody()->write(json_encode(['message' => 'Project deleted successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
     } else {
-        return $response->withStatus(500)->getBody()->write(json_encode(['error' => 'Failed to delete project']));
+        $response->getBody()->write(json_encode(['error' => 'Failed to delete project']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
     }
 });
 
-// Fly, my pretties!
-$app->run();
+// Get all employees
+$app->get('/v1/employees', function (Request $request, Response $response) use ($mysqli) {
+    $sql = "SELECT * FROM employees";
+    $result = $mysqli->query($sql);
 
-?>
+    $employees = [];
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $employees[] = $row;
+        }
+    }
+
+    $response->getBody()->write(json_encode($employees, JSON_PRETTY_PRINT));
+    return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+});
+
+// Get a single employee
+$app->get('/v1/employees/{id}', function (Request $request, Response $response, $args) use ($mysqli) {
+    $id = $args['id'];
+    $stmt = $mysqli->prepare("SELECT * FROM employees WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $employee = $result->fetch_assoc();
+
+    if ($employee) {
+        $response->getBody()->write(json_encode($employee, JSON_PRETTY_PRINT));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    } else {
+        $response->getBody()->write(json_encode(['error' => 'Employee not found']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
+    }
+});
+
+// Add a new employee
+$app->post('/v1/employees', function (Request $request, Response $response) use ($mysqli) {
+    $data = $request->getParsedBody();
+
+    $firstName = $data['first_name'];
+    $lastName = $data['last_name'];
+    $specialization = $data['specialization'];
+
+    $stmt = $mysqli->prepare("INSERT INTO employees (first_name, last_name, specialization) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $firstName, $lastName, $specialization);
+
+    if ($stmt->execute()) {
+        $response->getBody()->write(json_encode(['message' => 'Employee added successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    } else {
+        $response->getBody()->write(json_encode(['error' => 'Failed to add employee']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+// Update an employee
+$app->put('/v1/employees/{id}', function (Request $request, Response $response, $args) use ($mysqli) {
+    $id = $args['id'];
+    $data = $request->getParsedBody();
+
+    $firstName = $data['first_name'];
+    $lastName = $data['last_name'];
+    $specialization = $data['specialization'];
+
+    $stmt = $mysqli->prepare("UPDATE employees SET first_name = ?, last_name = ?, specialization = ? WHERE id = ?");
+    $stmt->bind_param("sssi", $firstName, $lastName, $specialization, $id);
+
+    if ($stmt->execute()) {
+        $response->getBody()->write(json_encode(['message' => 'Employee updated successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    } else {
+        $response->getBody()->write(json_encode(['error' => 'Failed to update employee']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+// Delete an employee
+$app->delete('/v1/employees/{id}', function (Request $request, Response $response, $args) use ($mysqli) {
+    $id = $args['id'];
+
+    $stmt = $mysqli->prepare("DELETE FROM employees WHERE id = ?");
+    $stmt->bind_param("i", $id);
+
+    if ($stmt->execute()) {
+        $response->getBody()->write(json_encode(['message' => 'Employee deleted successfully']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+    } else {
+        $response->getBody()->write(json_encode(['error' => 'Failed to delete employee']));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+// Run the Slim application
+$app->run();
