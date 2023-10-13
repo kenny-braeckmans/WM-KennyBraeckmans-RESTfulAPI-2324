c21af10dc0d97a8318f8c0545fc1e48f5502f59d
Kenny Braeckmans <kenny.braeckmans@student.odisee.be>
2023-10-13 08:58:48 +0200

    implement middleware, enabling CORS

--- api/api.php
+++ api/api.php
@@ -6,12 +6,38 @@ ini_set('display_errors', 1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
+use Slim\Routing\RouteContext;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/inc/config.php';
 
 $app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+
+// This middleware will append the response header Access-Control-Allow-Methods with all allowed methods
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+    $response = $handler->handle($request);
+
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    // Optional: Allow Ajax CORS requests with Authorization header
+    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+    return $response;
+});
+
+// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
+$app->addRoutingMiddleware();
 
 // Get all projects
 $app->get('/v1/projects', function (Request $request, Response $response) use ($mysqli) {
